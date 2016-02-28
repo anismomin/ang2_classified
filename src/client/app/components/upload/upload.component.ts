c60ad8fd1580bcb687d0fe283b7a1cc4a095b26f
anismomin <anismomin.dev@gmail.com>
2016-02-28 14:47:35 +0500

    added mew file uploader

---
+++ src/client/app/components/upload/upload.component.ts
@@ -0,0 +1,29 @@
+
+// import {Component} from 'angular2/core';
+// import {CORE_DIRECTIVES, FORM_DIRECTIVES, NgClass, NgStyle} from 'angular2/common';
+// import {FILE_UPLOAD_DIRECTIVES, FileUploader} from '../../../ng2-file-upload';
+
+// // webpack html imports
+// let template = require('./simple-demo.html');
+
+// // const URL = '/api/';
+// const URL = 'http://localhost:3000/post/create';
+
+// @Component({
+//   selector: 'simple-demo',
+//   template: template,
+//   directives: [FILE_UPLOAD_DIRECTIVES, NgClass, NgStyle, CORE_DIRECTIVES, FORM_DIRECTIVES]
+// })
+// export class SimpleDemo {
+//   private uploader: FileUploader = new FileUploader({ url: URL });
+//   private hasBaseDropZoneOver: boolean = false;
+//   private hasAnotherDropZoneOver: boolean = false;
+
+//   private fileOverBase(e: any) {
+//     this.hasBaseDropZoneOver = e;
+//   }
+
+//   private fileOverAnother(e: any) {
+//     this.hasAnotherDropZoneOver = e;
+//   }
+// }
